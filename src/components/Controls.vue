6a5b9ab8c3cb2aba7405e5ced58f81cc09a82a2d
CruidGals <kchiem30@gmail.com>
2025-11-07 12:31:23 -0500

    Add a simple keybind documentation modal

--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -2,6 +2,11 @@
 import Slider from './Slider.vue';
 import { turnSpeed } from '@/composables/cubeLogic';
 import { showFaceSymbols } from '@/composables/cubeVisual';
+import Modal from './Modal.vue';
+import { ref } from 'vue';
+
+const showModal = ref(false);
+
 </script>
 
 <template>
@@ -11,6 +16,42 @@ import { showFaceSymbols } from '@/composables/cubeVisual';
         <label class>
             Show Face Symbols <input type="checkbox" v-model="showFaceSymbols">
         </label>
+
+        <!-- Modal for keybinds -->
+        <teleport to="body">
+            <Modal v-model="showModal">
+                <div class="modal-content">
+                    <h1>Keybinds</h1>
+
+                    <div style="display: flex; gap: 20px; justify-content: center;">
+                        <div style="display: flex; flex-direction: column;">
+                            <p><span class="bold">R</span>: Move R face CW</p>
+                            <p><span class="bold">L</span>: Move L face CW</p>
+                            <p><span class="bold">F</span>: Move F face CW</p>
+                            <p><span class="bold">B</span>: Move B face CW</p>
+                            <p><span class="bold">U</span>: Move U face CW</p>
+                            <p><span class="bold">D</span>: Move D face CW</p>
+                        </div>
+                        <div style="display: flex; flex-direction: column;">
+                            <p><span class="bold">M</span>: Move the M layer CW</p>
+                            <p><span class="bold">S</span>: Move the S layer CW</p>
+                            <p><span class="bold">E</span>: Move the E layer CW</p>
+                        </div>
+                        <div style="display: flex; flex-direction: column;">
+                            <p><span class="bold">X</span>: Rotate cube CW from R Face</p>
+                            <p><span class="bold">Y</span>: Rotate cube CW from U Face</p>
+                            <p><span class="bold">Z</span>: Rotate cube CW from F Face</p>
+                        </div>
+                    </div>
+                    
+                    <div>
+                        <p>Holding <span class="bold">Shift</span> will reverse the direction to CCW.</p>
+                        <p>Holding <span class="bold">Left Ctrl</span> will perform wide moves.</p>
+                    </div>
+                </div>
+            </Modal>
+        </teleport>
+        <button @click="showModal = true">Show Keybinds</button>
     </div>
 </template>
 
@@ -26,5 +67,22 @@ import { showFaceSymbols } from '@/composables/cubeVisual';
         z-index: 10;
 
         padding: 20px;
+    }
+
+    .modal-content {
+        background-color: black;
+        color: white;
+        padding: 50px;
+        border-radius: 10px;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 20px;
+    }
+
+    .bold {
+        font-weight: 900;
     }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const isOpen = defineModel<boolean>({ default: false });
+
+function handleBackdropClick(event: MouseEvent) {
+    if (event.target === event.currentTarget) {
+        isOpen.value = false;
+    }
+}
+</script>
+
+<template>
+    <div class="backdrop" v-if="isOpen" @click="handleBackdropClick">
+        <slot></slot>
+    </div>
+</template>
+
+
+<style scoped>
+.backdrop {
+    position: fixed;
+    top: 0;
+    left: 0;
+    background-color: rgba(255,255,255,0.1);
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 1000;
+}
+</style>
