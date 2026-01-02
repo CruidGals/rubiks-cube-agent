42f7e84bff9308221e20c86c43dc11405eee471c
CruidGals <kchiem30@gmail.com>
2026-01-02 01:25:49 -0500

    Implement basic scramble generator

--- src/components/overlay/Timer.vue
+++ src/components/overlay/Timer.vue
@@ -1,12 +1,48 @@
 <script setup lang="ts">
 
+import { ref } from 'vue';
+import { randomScrambleForEvent } from "cubing/scramble";
+
+const props = defineProps<{
+    width?: string;
+}>();
+
+const scramble = ref('');
+
+async function generateScramble() {
+    let res = await randomScrambleForEvent("333");
+    scramble.value = res.toString();
+}
+
 </script>
 
 <template>
+    <div class="overlay" :style="{ width: props.width }">
+        <div class="button" @click="generateScramble">Generate Scramble</div>
 
+        <h3 class="scramble">{{ scramble }}</h3>
+    </div>
 </template>
 
 <style scoped>
+    .overlay {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 10px;
+
+        background-color: rgb(40, 40, 40);
+        border-radius: 0 0px 10px 0;
+        height: 100%;
+        padding: 20px;
+    }
+
+    .scramble {
+        font-size: 1.25rem;
+        font-weight: bold;
+        text-align: center;
+    }
+    
     .button {
         display: flex;
         align-items: center;
@@ -17,5 +53,10 @@
 
         width: 100%;
         transition: all 0.1s ease-out;
+    }
+
+    .button:hover {
+        background-color: rgb(80,80,80);
+        cursor: grab;
     }
 </style>
