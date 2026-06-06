153998e663d8a2ef1ad2a501d3284367c7782460
CruidGals <kchiem30@gmail.com>
2026-06-05 23:32:05 -0700

    Fix cfop state checker not checking right upon move completion

--- src/composables/notation/cubeNotation.ts
+++ src/composables/notation/cubeNotation.ts
@@ -1,6 +1,7 @@
 import { CubeMove, CubeNotation } from "../cubeLogic";
 import { computed, ref } from "vue";
 import { orient, permute } from "../util";
+import { checkCfopState } from "./cfopStateChecker";
 
 export enum Color {
     WHITE = 0,
@@ -378,6 +379,9 @@ export function updateCubeState(move: CubeMove) {
     orientCorners(cubeState.value, fixedMove);
     permuteEdges(cubeState.value, fixedMove);
     orientEdges(cubeState.value, fixedMove);
+
+    // Once everything is updated, update the CFOP state checker
+    checkCfopState(cubeState.value);
 }
 
 export function resetCubeState() {
