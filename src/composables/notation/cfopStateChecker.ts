23b46f5b22dde212c545d1fd77d6982c3fbd9e3b
CruidGals <kchiem30@gmail.com>
2026-06-05 23:28:00 -0700

    Fix typo

--- src/composables/notation/cfopStateChecker.ts
+++ src/composables/notation/cfopStateChecker.ts
@@ -52,8 +52,8 @@ function checkCrossSolved(state: CubeState) {
     if (cfopSolvedStates.cross.isSolved) return;
     
     // Check white cross first (UR, UF, UL, UB in right place)
-    if ((state.eo[0] == 0 && state.eo[1] == 1 && state.eo[2] == 2 && state.eo[3] == 3) &&
-        (state.ep[0] == 0 && state.ep[1] == 0 && state.ep[2] == 0 && state.ep[3] == 0)) {
+    if ((state.ep[0] == 0 && state.ep[1] == 1 && state.ep[2] == 2 && state.ep[3] == 3) &&
+        (state.eo[0] == 0 && state.eo[1] == 0 && state.eo[2] == 0 && state.eo[3] == 0)) {
 
         cfopSolvedStates.cross.isSolved = true;
         cfopSolvedStates.cross.crossColor = Color.WHITE;
@@ -61,8 +61,8 @@ function checkCrossSolved(state: CubeState) {
     }
 
     // Check green cross (UF, FR, DF, FL in right place)
-    if ((state.eo[1] == 1 && state.eo[8] == 8 && state.eo[5] == 5 && state.eo[9] == 9) &&
-        (state.ep[1] == 0 && state.ep[8] == 0 && state.ep[5] == 0 && state.ep[9] == 0)) {
+    if ((state.ep[1] == 1 && state.ep[8] == 8 && state.ep[5] == 5 && state.ep[9] == 9) &&
+        (state.eo[1] == 0 && state.eo[8] == 0 && state.eo[5] == 0 && state.eo[9] == 0)) {
 
         cfopSolvedStates.cross.isSolved = true;
         cfopSolvedStates.cross.crossColor = Color.GREEN;
@@ -70,8 +70,8 @@ function checkCrossSolved(state: CubeState) {
     }
 
     // Check orange cross (UL, FL, DL, BL in right place)
-    if ((state.eo[2] == 2 && state.eo[9] == 9 && state.eo[6] == 6 && state.eo[10] == 10) &&
-        (state.ep[2] == 0 && state.ep[9] == 0 && state.ep[6] == 0 && state.ep[10] == 0)) {
+    if ((state.ep[2] == 2 && state.ep[9] == 9 && state.ep[6] == 6 && state.ep[10] == 10) &&
+        (state.eo[2] == 0 && state.eo[9] == 0 && state.eo[6] == 0 && state.eo[10] == 0)) {
  
         cfopSolvedStates.cross.isSolved = true;
         cfopSolvedStates.cross.crossColor = Color.ORANGE;
@@ -79,8 +79,8 @@ function checkCrossSolved(state: CubeState) {
     }
 
     // Check blue cross (UB, BL, DB, BR in right place)
-    if ((state.eo[3] == 3 && state.eo[10] == 10 && state.eo[7] == 7 && state.eo[11] == 11) &&
-        (state.ep[3] == 0 && state.ep[10] == 0 && state.ep[7] == 0 && state.ep[11] == 0)) {
+    if ((state.ep[3] == 3 && state.ep[10] == 10 && state.ep[7] == 7 && state.ep[11] == 11) &&
+        (state.eo[3] == 0 && state.eo[10] == 0 && state.eo[7] == 0 && state.eo[11] == 0)) {
 
         cfopSolvedStates.cross.isSolved = true;
         cfopSolvedStates.cross.crossColor = Color.BLUE;
@@ -88,8 +88,8 @@ function checkCrossSolved(state: CubeState) {
     }
 
     // Check red cross (UR, FR, DR, BR in right place)
-    if ((state.eo[0] == 0 && state.eo[8] == 8 && state.eo[4] == 4 && state.eo[11] == 11) &&
-        (state.ep[0] == 0 && state.ep[8] == 0 && state.ep[4] == 0 && state.ep[11] == 0)) {
+    if ((state.ep[0] == 0 && state.ep[8] == 8 && state.ep[4] == 4 && state.ep[11] == 11) &&
+        (state.eo[0] == 0 && state.eo[8] == 0 && state.eo[4] == 0 && state.eo[11] == 0)) {
 
         cfopSolvedStates.cross.isSolved = true;
         cfopSolvedStates.cross.crossColor = Color.RED;
@@ -97,8 +97,8 @@ function checkCrossSolved(state: CubeState) {
     }
 
     // Check yellow cross (DR, DF, DL, DB in right place)
-    if ((state.eo[4] == 4 && state.eo[5] == 5 && state.eo[6] == 6 && state.eo[7] == 7) &&
-        (state.ep[4] == 0 && state.ep[5] == 0 && state.ep[6] == 0 && state.ep[7] == 0)) {
+    if ((state.ep[4] == 4 && state.ep[5] == 5 && state.ep[6] == 6 && state.ep[7] == 7) &&
+        (state.eo[4] == 0 && state.eo[5] == 0 && state.eo[6] == 0 && state.eo[7] == 0)) {
 
         cfopSolvedStates.cross.isSolved = true;
         cfopSolvedStates.cross.crossColor = Color.YELLOW;
