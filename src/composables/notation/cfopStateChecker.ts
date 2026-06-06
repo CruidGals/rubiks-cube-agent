e3b155b0850263d3ed5d64838823f14b97ee7f28
CruidGals <kchiem30@gmail.com>
2026-06-05 22:43:23 -0700

    Add check for cross sovled

--- src/composables/notation/cfopStateChecker.ts
+++ src/composables/notation/cfopStateChecker.ts
@@ -72,7 +72,7 @@ export function checkCrossSolved(state: CubeState) {
     // Check orange cross (UL, FL, DL, BL in right place)
     if ((state.eo[2] == 2 && state.eo[9] == 9 && state.eo[6] == 6 && state.eo[10] == 10) &&
         (state.ep[2] == 0 && state.ep[9] == 0 && state.ep[6] == 0 && state.ep[10] == 0)) {
-
+ 
         cfopSolvedStates.cross.isSolved = true;
         cfopSolvedStates.cross.crossColor = Color.ORANGE;
         return;
